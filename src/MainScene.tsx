56b9c8adfc2758220b3ae63fcd1789d53a9b1876
JF <jfan9988@gmail.com>
2021-01-24 21:21:14 +1100

    set up PBR

--- src/MainScene.tsx
+++ src/MainScene.tsx
@@ -1,7 +1,8 @@
-import React, { FunctionComponent } from "react";
-import { Engine, Scene } from "react-babylonjs";
-import { Color4, Vector3 } from "@babylonjs/core/Maths/math";
+import React, { FunctionComponent, useCallback, useState } from "react";
+import { Engine, Scene, SceneEventArgs } from "react-babylonjs";
+import { Color3, Color4, Vector3 } from "@babylonjs/core/Maths/math";
 import styled, { css } from "styled-components";
+import { BaseTexture, Texture } from "@babylonjs/core";
 
 const Wrapper = styled.div<MainSceneProps>`
   z-index: 99;
@@ -27,7 +28,24 @@ type MainSceneProps = {
   height: number;
 };
 
+let hdrTexture: BaseTexture | undefined = undefined;
+let hdrTextureClone: BaseTexture | undefined = undefined;
+const onSceneMounted = (createdArgs: SceneEventArgs) => {
+  createdArgs.scene.imageProcessingConfiguration.exposure = 0.6;
+  createdArgs.scene.imageProcessingConfiguration.contrast = 1.6;
+};
+
 const MainScene: FunctionComponent<MainSceneProps> = ({ width, height }) => {
+  const environmentUrl = "./images/environment.dds";
+  const [_, setTexturesLoaded] = useState(false);
+
+  const hdrTextureRef = useCallback((node) => {
+    hdrTexture = node;
+    hdrTextureClone = hdrTexture.clone();
+    hdrTextureClone.coordinatesMode = Texture.SKYBOX_MODE;
+    setTexturesLoaded(true); // trigger render and props assignment
+  }, []);
+
   return (
     <Wrapper width={width} height={height}>
       <Engine
@@ -37,7 +55,19 @@ const MainScene: FunctionComponent<MainSceneProps> = ({ width, height }) => {
         width={width}
         height={height}
       >
-        <Scene clearColor={new Color4(0, 0, 0, 0)}>
+        <Scene
+          clearColor={new Color4(0, 0, 0, 0)}
+          onSceneMount={onSceneMounted}
+        >
+          <cubeTexture
+            ref={hdrTextureRef}
+            name="hdrTexture"
+            rootUrl={environmentUrl}
+            createPolynomials={true}
+            format={undefined}
+            prefiltered={true}
+          />
+
           <freeCamera
             name="camera1"
             position={new Vector3(0, 5, -10)}
@@ -51,13 +81,43 @@ const MainScene: FunctionComponent<MainSceneProps> = ({ width, height }) => {
           />
 
           <sphere
-            name="sphere1"
-            diameter={2}
-            segments={16}
-            position={new Vector3(0, 1, 0)}
-          />
+            name="sphereGlass"
+            segments={48}
+            diameter={3}
+            translate={[new Vector3(0, 1, 0), 0.5]}
+          >
+            <pbrMaterial
+              name="glass"
+              reflectionTexture={hdrTexture}
+              linkRefractionWithTransparency
+              indexOfRefraction={0.52}
+              alpha={0}
+              microSurface={1}
+              reflectivityColor={new Color3(0.2, 0.2, 0.2)}
+              albedoColor={new Color3(0.85, 0.85, 0.85)}
+            />
+          </sphere>
 
-          <ground name="ground1" width={6} height={6} subdivisions={2} />
+          <ground name="ground1" width={6} height={6} subdivisions={2}>
+            <pbrMaterial
+              name="wood"
+              reflectionTexture={hdrTexture}
+              environmentIntensity={1}
+              specularIntensity={0.3}
+              albedoColor={Color3.White()}
+              useMicroSurfaceFromReflectivityMapAlpha
+            >
+              <texture
+                url="assets/textures/reflectivity.png"
+                assignTo="reflectivityTexture"
+              />
+
+              <texture
+                url="assets/textures/albedo.png"
+                assignTo="albedoTexture"
+              />
+            </pbrMaterial>
+          </ground>
         </Scene>
       </Engine>
     </Wrapper>
