4164997e30b9432918424cd3f2726eba69d294ae
Jane Fan <jane.fan@airtasker.com>
2021-01-30 18:56:17 +1100

    working simple example

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./App";
-import { initFaceDetect } from "./faceApi/faceDetect";
-import { initCamera } from "./faceApi/webcam";
+import * as faceapi from "face-api.js";
 
-const initAll = async () => {
-  ReactDOM.render(<App />, document.getElementById("app"));
-  await initCamera();
-  await initFaceDetect();
+const video = document.querySelector("#video");
+
+const startVideo = () => {
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then((mediaStream) => {
+      video.srcObject = mediaStream;
+    })
+    .catch((error) => console.log(error));
 };
 
-initAll();
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
+  faceapi.nets.faceLandmark68TinyNet.loadFromUri("./models"),
+]).then(startVideo);
+
+video.addEventListener("play", () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks(true);
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+  }, 100);
+});
