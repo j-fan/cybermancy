356694c959b9c79c6a664f574a3d7c6ada315a23
JF <jfan9988@gmail.com>
2021-02-14 13:18:43 +1100

    fix face api to babylon coord conversion

--- src/utils/faceApiToBabylonCoord.ts
+++ src/utils/faceApiToBabylonCoord.ts
@@ -2,6 +2,6 @@ import { Vector3 } from "@babylonjs/core";
 import { Point } from "@vladmandic/face-api";
 
 const faceApiToBabylonCoord = (faceApiCoord?: Point): Vector3 =>
-  new Vector3((faceApiCoord?.x ?? 0) * -1, faceApiCoord?.y, -100);
+  new Vector3((faceApiCoord?.x ?? 0) * -1, faceApiCoord?.y, 0);
 
 export { faceApiToBabylonCoord };
