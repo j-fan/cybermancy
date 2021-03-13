b552d65d952466bdbf27961f5c7d347c01ef9930
jfan <jfan9988@gmail.com>
2021-03-13 13:55:02 +1100

    add scan line effect

--- src/components/VideoOverlay.tsx
+++ src/components/VideoOverlay.tsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 import styled from "styled-components";
 import { gradient45deg } from "../globalStyles";
 
 const StyledVideoOverlay = styled.div`
   z-index: 1;
-  background: ${gradient45deg};
   width: 100%;
   height: 100%;
   position: absolute;
+`;
+
+const GradientOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  background: ${gradient45deg};
+  width: 100%;
+  height: 100%;
   opacity: 25%;
 `;
 
-const VideoOverlay = () => {
-  return <StyledVideoOverlay />;
+const Scanlines = styled.div`
+  position: absolute;
+  top: 0;
+  background: url("./images/scanlines.png");
+  width: 100%;
+  height: 100%;
+`;
+
+const VideoOverlay: FunctionComponent = () => {
+  return (
+    <StyledVideoOverlay>
+      <GradientOverlay />
+      <Scanlines />
+    </StyledVideoOverlay>
+  );
 };
 
 export { VideoOverlay };
